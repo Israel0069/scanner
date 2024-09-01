eada069fefdd83ed31e54e96ed903e0e39595388
Israel0069 <iraqi0069@gmail.com>
2024-09-01 11:21:11 +0800

    edit6

--- script.js
+++ script.js
@@ -11,31 +11,14 @@ function domReady(fn) {
 
 domReady(function () {
 
-    // If found your QR code
+    // If found you qr code
     function onScanSuccess(decodeText, decodeResult) {
-        // Display the QR code text in the input field
-        document.getElementById('qr-result').value = decodeText;
-
-        // Alert with the QR code text
-        alert("WTF QR code is: " + decodeText);
+        alert("Fuck Qr is : " + decodeText, decodeResult);
     }
 
     let htmlscanner = new Html5QrcodeScanner(
         "my-qr-reader",
-        { fps: 10, qrbox: 250 }
+        { fps: 10, qrbos: 250 }
     );
     htmlscanner.render(onScanSuccess);
-
-    // Copy to Clipboard button functionality
-    document.getElementById('copy-to-clipboard').addEventListener('click', function() {
-        let qrResult = document.getElementById('qr-result');
-        qrResult.select();
-        qrResult.setSelectionRange(0, 99999); // For mobile devices
-
-        navigator.clipboard.writeText(qrResult.value).then(function() {
-            alert('QR code text copied to clipboard!');
-        }, function(err) {
-            alert('Failed to copy QR code text: ', err);
-        });
-    });
 });
