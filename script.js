0fbb7d1851aa882f8e4bb6c495f14c82d8930a77
Israel0069 <iraqi0069@gmail.com>
2024-09-01 11:48:04 +0800

    edit12

--- script.js
+++ script.js
@@ -11,31 +11,14 @@ function domReady(fn) {
 
 domReady(function () {
 
-    // If found your QR code
+    // If found you qr code
     function onScanSuccess(decodeText, decodeResult) {
-        // Show the modal
-        const modal = document.getElementById('myModal');
-        const resultText = document.getElementById('qr-code-result');
-        resultText.textContent = "QR Code is: " + decodeText;
-        modal.style.display = "block";
-
-        // Close the modal when the user clicks on <span> (x)
-        const span = document.getElementsByClassName("close")[0];
-        span.onclick = function() {
-            modal.style.display = "none";
-        }
-
-        // Close the modal when the user clicks anywhere outside of the modal
-        window.onclick = function(event) {
-            if (event.target == modal) {
-                modal.style.display = "none";
-            }
-        }
+        alert("fuck Qr is : " + decodeText, decodeResult);
     }
 
     let htmlscanner = new Html5QrcodeScanner(
         "my-qr-reader",
-        { fps: 10, qrbox: 250 }
+        { fps: 10, qrbos: 250 }
     );
     htmlscanner.render(onScanSuccess);
 });
