43296b115b97b294e90508aadc9967ec756fc806
Israel0069 <iraqi0069@gmail.com>
2024-09-01 11:27:39 +0800

    edit9

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ domReady(function () {
 
     // If found you qr code
     function onScanSuccess(decodeText, decodeResult) {
-        alert("You Qr is : " + decodeText, decodeResult);
+        alert("Shit Qr is : " + decodeText, decodeResult);
     }
 
     let htmlscanner = new Html5QrcodeScanner(
